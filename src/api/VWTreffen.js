d6afa329f1d7eb4b8070402750a1f13eda03afe5
BolverBlitz <bolverblitz@ebg.pw>
2023-02-02 23:22:53 +0100

    Bugfix: Timevalidation did not work as intented

--- src/api/VWTreffen.js
+++ src/api/VWTreffen.js
@@ -71,7 +71,7 @@ const schemaPost = customJoi.object({
   Eventname: customJoi.string().max(256).htmlStrip().required().regex(/^[a-z\d\s\-\.\,\ä\ü\ö\ß\&\(\)\"\!\?\+\*\/\<\>\|]*$/i),
   Eventart: customJoi.number().max(3).required(),
   Date: customJoi.string().trim().required().regex(/^(?:(?:31(\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/),
-  Time: customJoi.string().trim().required().regex(/([01]?[0-9]|2[0-3]):[0-5][0-9]/),
+  Time: customJoi.string().trim().required().regex(/^([0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/),
   Adresse: customJoi.string().trim().max(256).htmlStrip().required().regex(/^[a-z\d\s\-\.\,\ä\ü\ö\ß\&\(\)\"\!\?\+\*\/\<\>\|]*$/i),
   URL: customJoi.string().max(256).htmlStrip().uri().allow(''),
   Beschreibung: customJoi.string().trim().max(1024).htmlStrip().required().regex(/^[a-z\d\s\-\.\,\ä\ü\ö\ß\&\(\)\"\!\?\+\*\/\<\>\|]*$/i),
